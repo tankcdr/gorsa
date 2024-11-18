f2f02e9025ca141f3d2533a8c544e493dcf0593e
Christopher Madison <chris_madison@bellsouth.net>
2024-11-18 13:23:19 -0500

    leverage eulers sieve to fidn  prime factors

--- cmd/factor/main.go
+++ cmd/factor/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/tankcdr/gorsa"
 )
 
 func findFactors(num int) []int {
@@ -13,14 +16,11 @@ func findFactors(num int) []int {
 		num /= factor
 	}
 
-	println(num)
-	factor = 3
-	for factor*factor <= num {
+	for factor = 3; factor*factor <= num; factor += 2 {
 		if num%factor == 0 {
 			factors = append(factors, factor)
 			num /= factor
 		}
-		factor += 2
 	}
 
 	if num > 1 {
@@ -30,7 +30,43 @@ func findFactors(num int) []int {
 	return factors
 }
 
+func findFactorsSieve(num int) []int {
+	factors := []int{}
+
+	factor := 2
+	for num%factor == 0 {
+		factors = append(factors, factor)
+		num /= factor
+	}
+
+	for index := 1; primes[index]*primes[index] <= num; index++ {
+		if num%primes[index] == 0 {
+			factors = append(factors, primes[index])
+			num /= primes[index]
+		}
+	}
+
+	if num > 1 {
+		factors = append(factors, num)
+	}
+
+	return factors
+}
+
+func multiplySlice(arr []int) int {
+	multiple := 1
+	for _, val := range arr {
+		multiple *= val
+	}
+	return multiple
+}
+
+var primes []int
+
 func main() {
+
+	primes = gorsa.SieveToPrimes(gorsa.SieveOfEuler(1700000000))
+
 	var numToFactor int
 	ok := true
 
@@ -41,8 +77,23 @@ func main() {
 		if numToFactor <= 2 {
 			ok = false
 		} else {
+			// Find the factors the slow way.
+			start := time.Now()
 			factors := findFactors(numToFactor)
-			fmt.Printf("%v\n", factors)
+			elapsed := time.Since(start)
+			fmt.Printf("findFactors:       %f seconds\n", elapsed.Seconds())
+			fmt.Println(multiplySlice(factors))
+			fmt.Println(factors)
+			fmt.Println()
+
+			// Use the Euler's sieve to find the factors.
+			start = time.Now()
+			factors = findFactorsSieve(numToFactor)
+			elapsed = time.Since(start)
+			fmt.Printf("findFactorsSieve: %f seconds\n", elapsed.Seconds())
+			fmt.Println(multiplySlice(factors))
+			fmt.Println(factors)
+			fmt.Println()
 		}
 	}
 }
